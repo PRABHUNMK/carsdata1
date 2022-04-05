74290075e80039bb1b867331e09ecb0ae6750db3
xlchandy <90453318+xlchandy@users.noreply.github.com>
2022-04-05 22:38:15 +0530

    Add files via upload

---
+++ cars.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import altair as alt
+import streamlit as st
+import psycopg2
+
+#Connect to the heroku postgresql database
+connection=psycopg2.connect(user="yfxpgallicmvhs",
+                            password="b42a0e5bddc705436e3b39271057b781cc398451ad2292d785d2311927cfbfa1",
+                            host="ec2-52-54-212-232.compute-1.amazonaws.com",
+                            database="dc802bu83sg0v6")
+
+#Create a cursor to perform database operations
+cursor=connection.cursor()
+
+
+#Execute SQL Query
+cursor.execute ("Select * from public.cars;")
+
+#Fetch result
+record=pd.DataFrame(cursor.fetchall())
+st.write(record)
+
+cursor.close()
+connection.close()
